cdea3c2ba13c88b9afeaa41fdcb3a5436c573fb1
Zander Mackie <zmackie@gmail.com>
2018-11-19 07:25:43 -0500

    Dealt with count data race

--- src/github.com/cmu440/p0/server_impl.go
+++ src/github.com/cmu440/p0/server_impl.go
@@ -3,7 +3,9 @@
 package p0
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"strings"
 )
@@ -14,6 +16,7 @@ type keyValueServer struct {
 	stop     chan interface{}
 	deadConn chan net.Conn
 	newConn  chan net.Conn
+	count    chan chan int
 	active   int
 	dropped  int
 }
@@ -26,57 +29,158 @@ func New() KeyValueServer {
 		newConn:  make(chan net.Conn),
 		deadConn: make(chan net.Conn),
 		stop:     make(chan interface{}),
+		count:    make(chan chan int),
 	}
 }
 
 func (kvs *keyValueServer) Start(port int) error {
-	// start server
-	go func() {
-		// accept loop
-	}()
-	// go accept loop
-	// for select channels
-	for {
-		select {
-		case conn := <-kvs.newConn:
-			kvs.clients[conn] = true
-			kvs.active++
-		case <-kvs.stop:
-		case conn := <-kvs.deadConn:
-			delete(kvs.clients, conn)
-			kvs.dropped++
-		case msg := <-kvs.msg:
-			msg.Run()
-		}
+	select {
+	case <-kvs.stop:
+		return fmt.Errorf("Cannot start server after its been started")
+	default:
+		break
 	}
+	init_db()
+
+	listener, err := net.Listen("tcp4", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return fmt.Errorf("problem starting : %v", err)
+	}
+	fmt.Println("Starting server on port :", port)
+
+	// start server
+	go func(l net.Listener) {
+	accept:
+		for {
+			fmt.Println("Starting accept loop", l.Addr())
+			select {
+			case <-kvs.stop:
+				fmt.Println("stopped")
+				break accept
+			default:
+				fmt.Println("continue")
+				break
+			}
+			fmt.Println("accepting")
+			conn, err := l.Accept()
+
+			if err != nil {
+				fmt.Println("err")
+				conn.Close()
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("accepting")
+			client := NewClient(conn, kvs)
+			go client.Start()
+			kvs.newConn <- conn
+		}
+	}(listener)
+
+	go func() {
+	logic:
+		for {
+			select {
+			case c := <-kvs.count:
+				c <- len(kvs.clients)
+			case conn := <-kvs.newConn:
+				fmt.Println("New conn", conn)
+				kvs.clients[conn] = true
+			case <-kvs.stop:
+				listener.Close()
+				for client, _ := range kvs.clients {
+					client.Close()
+					kvs.dropped++
+				}
+				kvs.active = 0
+				break logic
+			case conn := <-kvs.deadConn:
+				conn.Close()
+				delete(kvs.clients, conn)
+				kvs.dropped++
+			case cmd := <-kvs.msg:
+				cmd.Run()
+			}
+		}
+	}()
+
 	return nil
 }
 
 func (kvs *keyValueServer) Close() {
-	// close (this returns the zero value)
+	// close (this returns the zero value everywhere immediately)
 	close(kvs.stop)
 }
 
 func (kvs *keyValueServer) CountActive() int {
-	return kvs.active
+	c := make(chan int)
+	kvs.count <- c
+	return <-c
+
 }
 
 func (kvs *keyValueServer) CountDropped() int {
 	return kvs.dropped
 }
 
-// TODO: add additional methods/functions below!
 type client struct {
-	stop  chan interface{}
-	write chan [][]byte
-	msgs  chan CMD
-	conn  net.Conn
+	stop     chan interface{}
+	deadConn chan net.Conn
+	write    chan fetchRes
+	msgs     chan CMD
+	conn     net.Conn
 }
 
-func (cl client) Start() error {
-	// for select on write or stop
-	// go read from conn and write to msgs or close
-	return nil
+func NewClient(conn net.Conn, serv *keyValueServer) client {
+	fmt.Println("new client", conn)
+	return client{
+		stop:     serv.stop,
+		msgs:     serv.msg,
+		deadConn: serv.deadConn,
+		write:    make(chan fetchRes, 500),
+		conn:     conn,
+	}
+}
+
+func (cl client) Start() {
+	fmt.Println("Starting client", cl.conn.RemoteAddr())
+	go func() {
+		for {
+			msg, err := bufio.NewReader(cl.conn).ReadString('\n')
+			if err != nil {
+				fmt.Printf("err in client : %s", err)
+				if err == io.EOF {
+					cl.deadConn <- cl.conn
+					break
+				}
+				continue
+			}
+			cmd, err := cl.parseMsg(msg)
+			if err != nil {
+				fmt.Printf("err in client : %s", err)
+				continue
+			}
+			cl.msgs <- cmd
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case msg := <-cl.write:
+			for _, v := range msg.vals {
+				n, err := fmt.Fprintf(cl.conn, "%s,%s\n", msg.k, v)
+				if n == 0 || err != nil {
+					fmt.Printf("err in client : %s", err)
+					cl.deadConn <- cl.conn
+					break loop
+				}
+			}
+		case <-cl.stop:
+			break loop
+		}
+
+	}
 }
 
 func (cl client) parseMsg(msg string) (CMD, error) {
@@ -94,7 +198,7 @@ func (cl client) parseMsg(msg string) (CMD, error) {
 			v: []byte(req[2]),
 		}, nil
 	case "delete":
-		return delete{
+		return del{
 			k: req[1],
 		}, nil
 	default:
@@ -102,19 +206,23 @@ func (cl client) parseMsg(msg string) (CMD, error) {
 	}
 }
 
-// CMDs are sent by clients to the server
+// CMD is sent by clients to the server
 type CMD interface {
 	Run()
 }
 
 type fetch struct {
 	k     string
-	reply chan [][]byte
+	reply chan fetchRes
+}
+type fetchRes struct {
+	k    string
+	vals [][]byte
 }
 
 func (f fetch) Run() {
 	select {
-	case f.reply <- get(f.k):
+	case f.reply <- fetchRes{f.k, get(f.k)}:
 	default:
 		return
 	}
@@ -129,10 +237,10 @@ func (s set) Run() {
 	put(s.k, s.v)
 }
 
-type delete struct {
+type del struct {
 	k string
 }
 
-func (d delete) Run() {
+func (d del) Run() {
 	clear(d.k)
 }
